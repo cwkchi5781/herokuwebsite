e6363982a7953d4db4608532e46a9c213a57d3ef
cwkchi5781 <friendsrock2004chi@gmail.com>
2021-06-23 21:27:57 -0700

    First Commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, request, jsonify, redirect, session, url_for
+import gunicorn, mysql.connector
+from datetime import datetime
+
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=["POST", "GET"])
+def index():
+    return render_template("landing.html")
+
+if __name__ == '__main__':
+    app.run()
