babffd15c6f6146fef47ca3b11f2376472ceadee
cwkchi5781 <friendsrock2004chi@gmail.com>
2021-06-23 22:28:56 -0700

    forgot to change commit message, added a lot of html and css from a template

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=["POST", "GET"])
 def index():
-    return render_template("landing.html")
+    return render_template("landing.html", page="home")
 
 if __name__ == '__main__':
     app.run()
