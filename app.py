e707ca922bf0f30522abb7cd30d5869253b6488f
cwkchi5781 <friendsrock2004chi@gmail.com>
2021-06-24 04:09:08 -0700

    trying to see if database will work on heroku, cross your fingres

--- app.py
+++ app.py
@@ -4,7 +4,13 @@ from datetime import datetime
 
 
 app = Flask(__name__)
+#mysql://b0ccf8cd5f7945:27fb9f4e@us-cdbr-east-04.cleardb.com/heroku_bde763ff3f353c6?reconnect=true
 
+db = mysql.connector.connect(
+    host="cdbr-east-04.cleardb.com",
+    user="b0ccf8cd5f7945",
+    password="27fb9f4e",
+    database="heroku_bde763ff3f353c6")
 
 @app.route('/', methods=["POST", "GET"])
 def index():
