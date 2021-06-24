dd81a204b6477281217508df35da564842fc46dd
cwkchi5781 <friendsrock2004chi@gmail.com>
2021-06-24 04:10:52 -0700

    changed host name

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 #mysql://b0ccf8cd5f7945:27fb9f4e@us-cdbr-east-04.cleardb.com/heroku_bde763ff3f353c6?reconnect=true
 
 db = mysql.connector.connect(
-    host="cdbr-east-04.cleardb.com",
+    host="us-cdbr-east-04.cleardb.com",
     user="b0ccf8cd5f7945",
     password="27fb9f4e",
     database="heroku_bde763ff3f353c6")
