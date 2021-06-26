1b2ee72adba1d90f9b5f48e9eedc9368862fffa6
cwkchi5781 <friendsrock2004chi@gmail.com>
2021-06-26 14:00:47 -0700

    reverted to without defining transport as websocket on client side

--- static/scripts/socketiochat.js
+++ static/scripts/socketiochat.js
@@ -1,7 +1,7 @@
   $(function() {
       //var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
-    //var socket = io();
-    var socket = io({transports: ['websocket']});
+    var socket = io();
+    //var socket = io({transports: ['websocket']});
 
 
 
