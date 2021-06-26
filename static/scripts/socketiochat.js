0a68d1d88991e7bb26446eb8c9bac5ec5e2a513d
cwkchi5781 <friendsrock2004chi@gmail.com>
2021-06-25 23:01:23 -0700

    HAHAHAHAHHAHAHA FINALLY MAKE THE STUPID SOCKETIO WORK WITH ALL IT'S DEPEDNECES AND VERISONS, THE WEBSITE DOES ABSOLUTE NOTHING BUT BACK END AND SHIT HA! DONE FOR THE DAY GONNA MESS WITH STUFF TOMORROW ALSO SPENT TOO MUCH TIME MAKING FRONT END PRETTY

---
+++ static/scripts/socketiochat.js
@@ -0,0 +1,45 @@
+  $(function() {
+      //var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
+    var socket = io();
+
+
+    var username = null;
+
+    var wrapper = $("#live-chat-alert").parent("div.clearfix")
+    $("#live-chat-username-button").click(function() {
+      //console.log(event)
+     //event.preventDefault();
+     if ($("#username-input").val() == ''){
+        wrapper.addClass("alert alert-warning");
+        $( "#live-chat-alert" ).html("No username entered");
+     }else{
+         if(wrapper.hasClass( "alert" )){
+            wrapper.removeClass("alert alert-warning");
+            $( "#live-chat-alert" ).html("");
+         }
+
+          console.log("hi button stuff");
+
+          username = $("#username-input").val();
+            console.log("client emit");
+         socket.emit("sendusername", username);
+         //io.serverSideEmit('sendusername', {'username': username} );
+         //io.emit('sendusername', {'username': username} );
+
+
+     }
+       socket.on("comfirmusername", (usernamestatus) => {
+       console.log("working on client side");
+          if (usernamestatus == true){
+            $( "#live-chat-username" ).addClass( "hide" );
+            $( "#live-chat-message" ).removeClass( "hide" );
+          }else{
+            wrapper.addClass("alert alert-warning");
+            $( "#live-chat-alert" ).html("Username already taken");
+          }
+        });
+
+    });
+
+
+});
